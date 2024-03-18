fe4fd0d218e461b2692e67d71c1269a7883f115f
Keifer <144736945+kbuss26@users.noreply.github.com>
2024-03-18 11:41:44 -0600

    Create semi-working prototype
    Haven't explored all possible cases, need further testing by file and possible breadth-first search implementation

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
+/*
+    Keifer Buss 
+    COSC-3020-01 
+    Last modified Mar. 18 2024
+    Sources: none for this program
+*/
+
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    if (startNode === targetNode) return [startNode];
+    // For this algorithm, we will assume an adjacency matrix with a value of 1
+    // representing a path exists and 2 representing that it exists and has been visited
+    var pos = null;
+    var nodeList = [];
+
+    for (var i = 0; i < graph.length; i++) {
+        pos = graph[startNode][i];
+        if (pos === 1) {
+            if (i === targetNode) return [i];
+            graph[startNode][i] = 2;
+            nodeList = nodeList.concat(i, depthFirstSearch(graph, i, targetNode));
+        }
+        if (nodeList[nodeList.length - 1] != targetNode) {nodeList = []; }
+    }
+
+    return nodeList;
 }
